04f5e2d6389d2c726cdd263145b1426678599948
kerthiyashwanthrao <kerthiyashwanthrao@gmail.com>
2023-05-22 10:59:25 +0000

    fruitscounter

---
+++ src/components/FruitsCounter/index.js
@@ -0,0 +1,65 @@
+// Write your code here
+import './index.css'
+
+import {Component} from 'react'
+
+class FruitsCounter extends Component {
+  state = {mango: 0, banana: 0}
+
+  eatMango = () => {
+    this.setState(prevState => ({mango: prevState.mango + 1}))
+  }
+
+  eatBanana = () => {
+    this.setState(prevState => ({banana: prevState.banana + 1}))
+  }
+
+  render() {
+    const {mango, banana} = this.state
+    return (
+      <div className="app-container">
+        <div className="fruits-counter">
+          <h1 className="count-text">
+            Bob ate <span className="count">{mango}</span> mangoes
+            <span className="count"> {banana}</span> bananas
+          </h1>
+          <div className="counters-control-container">
+            <div className="counter-control">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/mango-img.png"
+                alt="mango"
+                className="fruit-image"
+              />
+              <div className="button-container">
+                <button
+                  type="button"
+                  className="button"
+                  onClick={this.eatMango}
+                >
+                  Eat Mango
+                </button>
+              </div>
+            </div>
+            <div className="counter-control">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/banana-img.png"
+                alt="banana"
+                className="fruit-image"
+              />
+              <div className="button-container">
+                <button
+                  type="button"
+                  className="button"
+                  onClick={this.eatBanana}
+                >
+                  Eat Banana
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default FruitsCounter
